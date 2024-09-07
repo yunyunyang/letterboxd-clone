874314907807c3604896ab95ba181a22e561ff3e
Chloe <yunyunya@gmail.com>
2024-09-07 22:15:15 +0200

    added tailwind css

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
+// https://vitejs.dev/guide/static-deploy.html#github-pages
 export default defineConfig({
   plugins: [vue()],
+  base: '/letterboxd-clone/',
 })
